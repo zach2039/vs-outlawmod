c949e8a6384c5d859a0df2f2bf171bbe50955647
User <User@DESKTOP-0OE6DN1>
2022-03-09 09:32:47 -0800

    Add project files.

---
+++ mods/outlawmod/src/Core.cs
@@ -0,0 +1,53 @@
+﻿
+    using System.Diagnostics;
+    using Vintagestory.API.Client;
+    using Vintagestory.API.Common;
+    using Vintagestory.API.Server;
+    using Vintagestory.GameContent;
+
+namespace OutlawMod
+{
+
+    public class Core : ModSystem
+    {
+        ICoreAPI api;
+
+        public override void Start(ICoreAPI api)
+        {
+            this.api = api;
+
+            base.Start(api);
+
+            //Debug.WriteLine("Outlaw Mod Started Sucessfully.");
+
+            //This is where we register our shoot arrow ai behavor once we add it.
+            //Example can be found in the VSSurvival Code in AiTaskThrowAtEntity.cs.
+
+            RegisterEntities();
+            RegisterEntityBehaviors();
+            RegisterAiTasks();
+
+            //api.ModLoader.GetModSystem(  )
+            //api.ObjectCache.
+
+
+
+        }
+    
+        private void RegisterEntities()
+        {
+            
+        }
+
+        private void RegisterEntityBehaviors()
+        {
+            //This is where we register our harvestable behavior.
+            //api.RegisterEntityBehaviorClass("harvestable", typeof(OutlawEntityBehaviorHarvestable));
+        }
+
+        private void RegisterAiTasks()
+        {
+            AiTaskRegistry.Register("shootatentity", typeof(AiTaskShootProjectileAtEntity));
+        }
+    }
+}
---
+++ mods/outlawmod/src/OutlawEntityBehaviorHarvestable.cs
@@ -0,0 +1,23 @@
+﻿
+    using System.Diagnostics;
+    using System.Text;
+    using Vintagestory.API.Common.Entities;
+    using Vintagestory.GameContent;
+
+namespace OutlawMod
+{    
+
+    public class OutlawEntityBehaviorHarvestable : EntityBehaviorHarvestable
+    {
+        public OutlawEntityBehaviorHarvestable(Entity entity) : base(entity)
+        {
+
+        }
+
+        public override void GetInfoText(StringBuilder infotext)
+        {
+            //Todo: Figure Out how we can register this.
+        }
+    }
+}
+
