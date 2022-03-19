103dcfb9bacd2eb05b26e579b598686e9cdf5f8a
Grifthegnome <grifbd@aol.com>
2022-03-18 23:07:13 -0700

    Drifter Overhaul Mod - Restored Harvesting To Outlaws to Fix Crash
    DRIFTER OVERHAUL MOD
    1. Mod blocks drifters from spawning on surface of ground.
    
    OUTLAWS WERE CRASHING WHEN HARVESTED
    1. This is now fixed.
    2. They have to drop one item, so they drop linnen.

---
+++ mods/drifteroverhaul/src/Core.cs
@@ -0,0 +1,24 @@
+﻿
+using System.Diagnostics;
+using System.Reflection;
+using Vintagestory.API.Client;
+using Vintagestory.API.Common;
+using Vintagestory.API.Server;
+using Vintagestory.GameContent;
+
+namespace DrifterOverhaulMod
+{
+
+    public class Core : ModSystem
+    {
+        ICoreAPI api;
+
+        public override void Start(ICoreAPI api)
+        {
+            this.api = api;
+
+            base.Start(api);
+
+        }
+    }
+}
