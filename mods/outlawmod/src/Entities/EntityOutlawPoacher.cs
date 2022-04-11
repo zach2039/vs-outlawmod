ad599a2fb0773ad5810d0506b5bd8cee83156b1c
Grifthegnome <grifbd@aol.com>
2022-04-11 11:56:20 -0700

    Bug Fix For Hunting Hounds Spawning In Geo 1. Hounds spawning on a poacher will now defer their spawn if they would spawn intersecting geo.

--- mods/outlawmod/src/Entities/EntityOutlawPoacher.cs
+++ mods/outlawmod/src/Entities/EntityOutlawPoacher.cs
@@ -4,6 +4,7 @@ using System.Diagnostics;
 using System.Collections.Generic;
 using Vintagestory.API.Common;
 using Vintagestory.API.Common.Entities;
+using Vintagestory.API.MathTools;
 using Vintagestory.GameContent;
 using ExpandedAiTasks;
 
@@ -11,6 +12,7 @@ namespace OutlawMod
 {
     public class EntityOutlawPoacher : EntityOutlaw
     {
+        List<long> callbacks = new List<long>();
 
         const int HOUND_COMPANIONS_MIN = 1;
         const int HOUND_COMPANIONS_MAX = 3;
@@ -26,25 +28,51 @@ namespace OutlawMod
 
             for( int i = 0; i < houndToSpawn; i++ )
             {
-                AssetLocation code = new AssetLocation("hound-hunting");
-                EntityProperties houndProperties = this.World.GetEntityType(code);
-
-                Debug.Assert(houndProperties != null, "Hound Properties are null");
-
-                Entity houndEnt = this.World.ClassRegistry.CreateEntity(houndProperties);
-                houndEnt.ServerPos.SetFrom(this.ServerPos);
-                houndEnt.Pos.SetFrom(houndEnt.ServerPos);
-
-                if (houndEnt is EntityAgent)
-                {
-                    EntityAgent houndAgent = (EntityAgent)houndEnt;
-                    houndAgent.HerdId = this.HerdId;
-
-                    AiUtility.SetGuardedEntity(houndAgent, this);        
-                }
-
-                this.World.SpawnEntity(houndEnt);
+                AttemptSpawnHuntingHound(0f);
             }
         }
-    }
+
+        protected void AttemptSpawnHuntingHound(float dt)
+        {
+            AssetLocation code = new AssetLocation("hound-hunting");
+            EntityProperties houndProperties = this.World.GetEntityType(code);
+
+            Debug.Assert(houndProperties != null, "Hound Properties are null");
+
+            Cuboidf collisionBox = houndProperties.SpawnCollisionBox;
+
+            // Delay hound spawning if we're colliding
+            if (this.World.CollisionTester.IsColliding(this.World.BlockAccessor, collisionBox, this.ServerPos.XYZ, false))
+            {
+                long callbackId = this.World.RegisterCallback(AttemptSpawnHuntingHound, 3000);
+                callbacks.Add(callbackId);
+                return;
+            }
+
+            Entity houndEnt = this.World.ClassRegistry.CreateEntity(houndProperties);
+            houndEnt.ServerPos.SetFrom(this.ServerPos);
+            houndEnt.Pos.SetFrom(houndEnt.ServerPos);
+
+            if (houndEnt is EntityAgent)
+            {
+                EntityAgent houndAgent = (EntityAgent)houndEnt;
+                houndAgent.HerdId = this.HerdId;
+
+                AiUtility.SetGuardedEntity(houndAgent, this);
+            }
+
+            this.World.SpawnEntity(houndEnt);
+        }
+
+        public override void OnEntityDespawn(EntityDespawnReason despawn)
+        {
+            base.OnEntityDespawn(despawn);
+
+            foreach( long callbackId in callbacks )
+            {
+                this.World.UnregisterCallback(callbackId);
+            }
+        }
+
+    }   
 }
